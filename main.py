39f8f06028deb737d8913a966d606d917f8143e0
ericamcbrayer <emcbrayer@ufl.edu>
2023-10-25 12:11:33 -0400

    erica lab 6

--- main.py
+++ main.py
@@ -1,16 +1,31 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+og_password = ''
+encoded_password = ''
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+def menu_display():
+    print("Menu")
+    print("-------------")
+    print("1. Encode\n2. Decode\n3. Quit")
+    print()
 
+# menu display and main code
+def main():
+    continue_menu = True
+    while continue_menu:
+        menu_display()
+        menu_option = input("Please enter an option: ")
+        if menu_option == '1':
+            og_password = input("Please enter your password to encode: ")
+            encoded_password = ""
+            for digit in og_password:
+                encoded_digit = str((int(digit) + 3) % 10)
+                encoded_password += encoded_digit
+            print("Your password has been encoded and stored!")
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+        elif menu_option == '2':
+            pass
+        elif menu_option == '3':
+            continue_menu = False
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+if __name__ == "__main__":
+    main()
